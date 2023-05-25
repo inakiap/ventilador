76ade857ec9a526b21888e21e41a3871e31cf1eb
Iñaki Alfaro <inakiap@gmail.com>
2023-05-25 22:34:18 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    activo = true
+})
+input.onButtonPressed(Button.B, function () {
+    activo = false
+})
+let activo = false
+edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+basic.forever(function () {
+    if (edubitPotentioBit.comparePot(PotCompareType.MoreThan, 5) && activo) {
+        edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Forward, edubitPotentioBit.readPotValue())
+        edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    } else {
+        edubitMotors.brakeMotor(MotorChannel.M1)
+        edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    }
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.No)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+})
